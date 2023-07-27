ad17577d883cba9db156e6f2fd37d81f6d6028d5
missfatimaa <fatemehjaafari3672@gmail.com>
2023-07-27 20:49:27 +0430

    finishing second page

--- java.js
+++ java.js
@@ -1,2 +1,28 @@
 let $ = document
-    
+let morfyGif = $.querySelector("#oppenheimer")
+let clone = $.querySelector("#clone")
+let camp = $.querySelector("#camp")
+
+
+
+morfyGif.addEventListener('mouseenter', function () {
+        morfyGif.src = "/images/morphy3.webp"  
+})
+
+morfyGif.addEventListener('mouseleave', function () {
+    morfyGif.src = "/images/morphy2.jpg"
+})
+
+clone.addEventListener('mouseenter', function () {
+    clone.src = "/images/cloned.gif"
+})
+clone.addEventListener('mouseleave', function () {
+    clone.src = "/images/cloned.png"
+})
+
+camp.addEventListener('mouseenter', function () {
+    camp.src = "/images/camp.gif"
+})
+camp.addEventListener('mouseleave', function () {
+    camp.src = "/images/camp.jpg"
+})
