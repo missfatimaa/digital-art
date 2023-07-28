26e0f6b42f7d6209556cf3ecca4926899807fb1f
missfatimaa <fatemehjaafari3672@gmail.com>
2023-07-28 22:00:17 +0430

    fifth layer

--- java.js
+++ java.js
@@ -2,11 +2,11 @@ let $ = document
 let morfyGif = $.querySelector("#oppenheimer")
 let clone = $.querySelector("#clone")
 let camp = $.querySelector("#camp")
-
-
-
+let email = $.querySelector("#Email")
+let subscribe = $.querySelector("#subBtn")
+let emailSec = $.querySelector(".email-input")
 morfyGif.addEventListener('mouseenter', function () {
-        morfyGif.src = "/images/morphy3.webp"  
+    morfyGif.src = "/images/morphy3.webp"
 })
 
 morfyGif.addEventListener('mouseleave', function () {
@@ -25,4 +25,26 @@ camp.addEventListener('mouseenter', function () {
 })
 camp.addEventListener('mouseleave', function () {
     camp.src = "/images/camp.jpg"
+})
+email.addEventListener("focus", function () {
+    emailSec.classList.add("box-shadow")
+})
+email.addEventListener("focusout", function () {
+    emailSec.classList.remove("box-shadow")
+})
+
+subscribe.addEventListener("click", function (event) {
+    event.preventDefault()
+    console.log(email.value);
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "<sender’s email address>",
+        Password: "<email password>",
+        To: '<recipient’s email address>',
+        From: "<sender’s email address>",
+        Subject: "<email subject>",
+        Body: "<email body>",
+    }).then(
+        message => alert("mail sent successfully")
+    );
 })
