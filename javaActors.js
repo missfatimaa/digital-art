244cd353560650638e5bd9b10310cf9af5a596d8
missfatimaa <fatemehjaafari3672@gmail.com>
2023-08-03 18:29:18 +0430

    actors page footer & navbar

--- javaActors.js
+++ javaActors.js
@@ -5,7 +5,7 @@ let prevBtn = $.querySelector(".prev")
 let nextBtn = $.querySelector(".next")
 let imgSrc = ['/images/Robert-Downey-Jr.-Smile.jpg', '/images/sandra.jpg', '/images/bradPittjpg.jpg', '/images/cate.jpg', '/images/keanu.webp', '/images/anne.webp', '/images/Benedict.png']
 let imgIndex = 0
-let actorsNameArray = ['Robert Downey Jr.','Sandra Bullock', 'Brad Pitt', 'Cate Blanchett', 'Keanu Reeves', 'Anne Hathaway','Benedict Cumberbatch']
+let actorsNameArray = ['Robert Downey Jr.', 'Sandra Bullock', 'Brad Pitt', 'Cate Blanchett', 'Keanu Reeves', 'Anne Hathaway', 'Benedict Cumberbatch']
 let actorsName = $.querySelector(".txt p")
 
 function prevImage() {
@@ -27,8 +27,42 @@ function nextImage() {
     image.setAttribute("src", imgSrc[imgIndex])
     actorsName.innerHTML = actorsNameArray[imgIndex]
 }
-setInterval(nextImage, 3000)
+setInterval(nextImage, 5000)
 prevBtn.addEventListener("click", prevImage)
 nextBtn.addEventListener("click", nextImage)
 
 
+let rangeItem = $.getElementById("range")
+rangeItem.addEventListener("change", changeHandler)
+let zero = $.querySelector(".far")
+let hundred = $.querySelector(".fas")
+let brightnessFilter = $.querySelector(".brightnessFilter")
+let brightnessBox = $.querySelector(".brightness-box")
+
+function changeHandler(event) {
+    brightnessFilter.style.filter = "brightness(" + event.target.value + "%)"
+}
+zero.addEventListener("click", zeroHandler)
+hundred.addEventListener("click", hundredHandler)
+
+function zeroHandler() {
+    brightnessFilter.style.filter = "brightness(30%)"
+}
+function hundredHandler() {
+    brightnessFilter.style.filter = "brightness(100%)"
+
+}
+rangeItem.classList.add("unscaled")
+
+brightnessBox.addEventListener("mouseenter", function () {
+    rangeItem.classList.remove("unscaled")
+    rangeItem.classList.add("scaled")
+    zero.style.transform = "translateX(0)"
+    hundred.style.transform = "translateX(0)"
+})
+brightnessBox.addEventListener("mouseleave", function () {
+    rangeItem.classList.remove("scaled")
+    rangeItem.classList.add("unscaled")
+    zero.style.transform = "translateX(100px)"
+    hundred.style.transform = "translateX(-160px)"
+})
