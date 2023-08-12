2dcf053f2fb8ce4d71f60085a17d590bc4b00c74
missfatimaa <fatemehjaafari3672@gmail.com>
2023-08-13 00:56:37 +0430

    categorize js files

---
+++ js/javaMovie.js
@@ -0,0 +1,35 @@
+let $ = document
+let rangeItem = $.getElementById("range")
+rangeItem.addEventListener("change", changeHandler)
+let zero = $.querySelector(".dark")
+let hundred = $.querySelector(".light")
+let brightnessFilter = $.querySelector(".brightnessFilter")
+let brightnessBox = $.querySelector(".brightness-box")
+
+function changeHandler(event) {
+    brightnessFilter.style.filter = "brightness(" + event.target.value + "%)"
+}
+zero.addEventListener("click", zeroHandler)
+hundred.addEventListener("click", hundredHandler)
+
+function zeroHandler() {
+    brightnessFilter.style.filter = "brightness(30%)"
+}
+function hundredHandler() {
+    brightnessFilter.style.filter = "brightness(100%)"
+
+}
+rangeItem.classList.add("unscaled")
+
+brightnessBox.addEventListener("mouseenter", function () {
+    rangeItem.classList.remove("unscaled")
+    rangeItem.classList.add("scaled")
+    zero.style.transform = "translateX(0)"
+    hundred.style.transform = "translateX(0)"
+})
+brightnessBox.addEventListener("mouseleave", function () {
+    rangeItem.classList.remove("scaled")
+    rangeItem.classList.add("unscaled")
+    zero.style.transform = "translateX(100px)"
+    hundred.style.transform = "translateX(-160px)"
+})
