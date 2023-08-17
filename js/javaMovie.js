c15fb9c600c8dca023a5f0e98a50fcdf7ac0927e
missfatimaa <fatemehjaafari3672@gmail.com>
2023-08-17 16:03:38 +0430

    movie slider description

--- js/javaMovie.js
+++ js/javaMovie.js
@@ -14,18 +14,32 @@ let posters = ['/images/oppenheimer-movie-poster.jpg', '/images/jumanji.jpg', '/
 let imdbArray = ['⭐8.6', '⭐6.9', '⭐6.4', '⭐6.8']
 let timeLineArray = ['3h', '1h 59m', '1h 48m', '1h 56m']
 let genreArray = ['Drama, Thriller', 'Adventure, Comedy', 'Drama, Comedy', 'Action, Thriller']
+let movieYearArray = ['2023', '2017', '2022', '2020']
+let movieNameArray = ['Oppenheimer', 'Jumanji(Welcome To The Jungle)', 'Showing Up', 'Extraction']
+const movieDesArray = [
+    'During World War II, Lt. Gen. Leslie Groves Jr. appoints physicist J. Robert Oppenheimer to work on the top-secret Manhattan Project. Oppenheimer and a team of scientists spend years developing and designing the atomic bomb.',
+    'When four students play with a magical video game, they are drawn to the jungle world of Jumanji, where they are trapped as their avatars. To return to the real world, they must finish the game.',
+    'Showing Up is a 2022 American comedy-drama film co-written and directed by Kelly Reichardt, in her fourth collaboration with actress Michelle Williams. The film follows a sculptor managing the competing attentions of her art, job, family, and friendships.',
+    'A black-market mercenary who has nothing to lose is hired to rescue the kidnapped son of an imprisoned international crime lord. But in the murky underworld of weapons dealers and drug traffickers, an already deadly mission approaches the impossible.'
+];
 let videoIndex = 0
 let rightMovie = $.querySelector(".right-movie img")
 let leftMovie = $.querySelector(".left-movie img")
-let movieNameArray = ['Oppenheimer', 'Jumanji(Welcome To The Jungle)', 'Showing Up', 'Extraction']
 let movieName = $.querySelector("#movie-name")
 let movieNameMob = $.querySelector("#movie-name-mobile")
 let posterIndex = 0
 let imdb = $.querySelector(".movie-info :first-child")
+let imdbMob = $.querySelector(".movie-info-mobile :first-child")
 let timeLine = $.querySelector(".movie-info :nth-child(2)")
+let timeLineMob = $.querySelector(".movie-info-mobile :nth-child(2)")
 let genre = $.querySelector(".movie-info :nth-child(4)")
+let genreMob = $.querySelector(".movie-info-mobile :nth-child(4)")
+let movieYear = $.querySelector(".movie-info :nth-child(6)")
+let movieYearMob = $.querySelector(".movie-info-mobile :nth-child(6)")
 let leftArrow = $.querySelector(".fa-chevron-left")
 let rightArrow = $.querySelector(".fa-chevron-right")
+let movieDesMob = $.querySelector(".movie-info-box-mobile p")
+let movieDes = $.querySelector(".movie-info-box p")
 
 window.addEventListener("DOMContentLoaded", function () {
     $.documentElement.style.setProperty("--second-color", localStorage.getItem("color"))
@@ -50,11 +64,20 @@ function prevVideo(event) {
     console.log("prev");
     video.setAttribute("src", videoSrc[videoIndex])
     video.play()
-    movieName.innerHTML = movieNameArray[videoIndex]
     leftMovie.style.transform = "scale(1)"
+    movieName.innerHTML = movieNameArray[videoIndex]
+    movieNameMob.innerHTML = movieNameArray[videoIndex]
     imdb.innerHTML = imdbArray[videoIndex]
+    imdbMob.innerHTML = imdbArray[videoIndex]
     timeLine.innerHTML = timeLineArray[videoIndex]
+    timeLineMob.innerHTML = timeLineArray[videoIndex]
     genre.innerHTML = genreArray[videoIndex]
+    genreMob.innerHTML = genreArray[videoIndex]
+    movieYear.innerHTML = movieYearArray[videoIndex]
+    movieYearMob.innerHTML = movieYearArray[videoIndex]
+    movieDes.innerHTML = movieDesArray[videoIndex]
+    movieDesMob.innerHTML = movieDesArray[videoIndex]
+    // movieDes.innerHTML, movieDesMob.innerHTML = movieDesArray[videoIndex]
     // leftMovie.setAttribute("src", posters[videoIndex-1])
     // rightMovie.setAttribute("src", posters[videoIndex])
 }
@@ -71,11 +94,19 @@ function nextVideo(event) {
     }
     video.setAttribute("src", videoSrc[videoIndex])
     video.play()
-    movieName.innerHTML = movieNameArray[videoIndex]
     rightMovie.style.transform = "scale(1)"
+    movieName.innerHTML = movieNameArray[videoIndex]
+    movieNameMob.innerHTML = movieNameArray[videoIndex]
     imdb.innerHTML = imdbArray[videoIndex]
+    imdbMob.innerHTML = imdbArray[videoIndex]
     timeLine.innerHTML = timeLineArray[videoIndex]
+    timeLineMob.innerHTML = timeLineArray[videoIndex]
     genre.innerHTML = genreArray[videoIndex]
+    genreMob.innerHTML = genreArray[videoIndex]
+    movieYear.innerHTML = movieYearArray[videoIndex]
+    movieYearMob.innerHTML = movieYearArray[videoIndex]
+    movieDes.innerHTML = movieDesArray[videoIndex]
+    movieDesMob.innerHTML = movieDesArray[videoIndex]
     // leftMovie.setAttribute("src", posters[videoIndex - 1])
     // rightMovie.setAttribute("src", posters[videoIndex + 1])
 }
