34b856683c92907edc54523ad0d853847b8b91ff
missfatimaa <fatemehjaafari3672@gmail.com>
2023-08-18 20:50:23 +0430

    movie page adding posters

--- js/javaMovie.js
+++ js/javaMovie.js
@@ -9,19 +9,20 @@ let video = $.querySelector("#video")
 let playerIcon = $.querySelector(".video-icon")
 let volumeUp = $.querySelector(".volume-up i")
 let mute = $.querySelector(".mute i")
-let videoSrc = ['/video/Oppenheimer.mp4', '/video/thyron.mp4', '/video/theater.mp4','/video/Heart_of_Stone.mp4', '/video/extraction.mp4']
-let posters = ['/images/oppenheimer-movie-poster.jpg', '/images/cloned poster.jpg', '/images/Theater poster.jpg','/images/heart-of-stone.jpg', '/images/extraction.jpg']
-let imdbArray = ['⭐8.6', '⭐6.7', '⭐7.5','⭐5.7', '⭐7']
-let timeLineArray = ['3h', '2h 2m', '1h 33m','2h 3m', '2h 3m']
-let genreArray = ['Drama, Thriller', 'Mystery, Fantasy', 'Comedy','Mystery', 'Action, Thriller']
-let movieYearArray = ['2023', '2023', '2023', '2023','2023']
-let movieNameArray = ['Oppenheimer', 'They Cloned Tyrone', 'Theater Camp','Heart of Stone', 'Extraction 2']
+let videoSrc = ['/video/Oppenheimer.mp4', '/video/thyron.mp4', '/video/theater.mp4', '/video/Heart_of_Stone.mp4', '/video/extraction.mp4', '/video/freedom.mp4']
+let posters = ['/images/oppenheimer-movie-poster.jpg', '/images/cloned poster.jpg', '/images/Theater poster.jpg', '/images/heart-of-stone.jpg', '/images/extraction.jpg', '/images/freedom.jpg']
+let imdbArray = ['⭐8.6', '⭐6.7', '⭐7.5', '⭐5.7', '⭐7', '⭐7.8']
+let timeLineArray = ['3h', '2h 2m', '1h 33m', '2h 3m', '2h 3m', ' 2h 15m']
+let genreArray = ['Drama, Thriller', 'Mystery, Fantasy', 'Comedy', 'Mystery', 'Action, Thriller', 'Drama, Thriller']
+let movieYearArray = ['2023', '2023', '2023', '2023', '2023', '2023']
+let movieNameArray = ['Oppenheimer', 'They Cloned Tyrone', 'Theater Camp', 'Heart of Stone', 'Extraction 2', 'Sound of Freedom']
 const movieDesArray = [
     'During World War II, Lt. Gen. Leslie Groves Jr. appoints physicist J. Robert Oppenheimer to work on the top-secret Manhattan Project. Oppenheimer and a team of scientists spend years developing and designing the atomic bomb.',
     'They Cloned Tyrone is a 2023 American science fiction comedy mystery film directed by Juel Taylor, in his feature film directorial debut, from a screenplay he wrote with Tony Rettenmaier.',
     'Theater Camp is a 2023 American comedy film directed by Molly Gordon and Nick Lieberman in both of their respective feature directorial debuts, from a screenplay by Gordon, Lieberman, Ben Platt and Noah Galvin.',
     'Rachel Stone is an intelligence operative, the only woman who stands between her powerful global peacekeeping organization and the loss of its most valuable -- and dangerous -- asset.',
-    'Extraction 2 (stylised on-screen as Extraction II) is a 2023 American action thriller film directed by Sam Hargrave and written by Joe Russo, based on the graphic novel Ciudad by Ande Parks, Joe Russo, Anthony Russo, Fernando León González, and Eric Skillman.'
+    'Extraction 2 (stylised on-screen as Extraction II) is a 2023 American action thriller film directed by Sam Hargrave and written by Joe Russo, based on the graphic novel Ciudad by Ande Parks, Joe Russo, Anthony Russo, Fernando León González, and Eric Skillman.',
+    'After rescuing a boy from ruthless child traffickers, a federal agent learns the boys sister is still captive and decides to embark on a dangerous mission to save her.With time running out, he quits his job and journeys deep into the Colombian jungle, putting his life on the line to free her from a fate worse than death.'
 ];
 let videoIndex = 0
 let rightMovie = $.querySelector(".right-movie img")
@@ -75,7 +76,7 @@ function videoAnimation() {
     interval
 }
 
-function movieInfoFiller(){
+function movieInfoFiller() {
     playerIcon.classList.remove("fa-play-circle")
     playerIcon.classList.add("fa-pause-circle")
     playerIcon.id = "pause-icon"
