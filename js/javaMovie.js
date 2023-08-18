0d7612675a0e4fdab12cb9940fe895e3e38c005c
missfatimaa <fatemehjaafari3672@gmail.com>
2023-08-18 17:38:58 +0430

    changing and adding the movies

--- js/javaMovie.js
+++ js/javaMovie.js
@@ -9,18 +9,19 @@ let video = $.querySelector("#video")
 let playerIcon = $.querySelector(".video-icon")
 let volumeUp = $.querySelector(".volume-up i")
 let mute = $.querySelector(".mute i")
-let videoSrc = ['/video/Oppenheimer.mp4', '/video/jumanji.mp4', '/video/showing up.mp4', '/video/extraction.mp4']
-let posters = ['/images/oppenheimer-movie-poster.jpg', '/images/jumanji.jpg', '/images/showing.jpg', '/images/extraction.jpg']
-let imdbArray = ['⭐8.6', '⭐6.9', '⭐6.4', '⭐6.8']
-let timeLineArray = ['3h', '1h 59m', '1h 48m', '1h 56m']
-let genreArray = ['Drama, Thriller', 'Adventure, Comedy', 'Drama, Comedy', 'Action, Thriller']
-let movieYearArray = ['2023', '2017', '2022', '2020']
-let movieNameArray = ['Oppenheimer', 'Jumanji (Welcome To The Jungle)', 'Showing Up', 'Extraction']
+let videoSrc = ['/video/Oppenheimer.mp4', '/video/thyron.mp4', '/video/theater.mp4','/video/Heart_of_Stone.mp4', '/video/extraction.mp4']
+let posters = ['/images/oppenheimer-movie-poster.jpg', '/images/cloned poster.jpg', '/images/Theater poster.jpg','/images/heart-of-stone.jpg', '/images/extraction.jpg']
+let imdbArray = ['⭐8.6', '⭐6.7', '⭐7.5','⭐5.7', '⭐7']
+let timeLineArray = ['3h', '2h 2m', '1h 33m','2h 3m', '2h 3m']
+let genreArray = ['Drama, Thriller', 'Mystery, Fantasy', 'Comedy','Mystery', 'Action, Thriller']
+let movieYearArray = ['2023', '2023', '2023', '2023','2023']
+let movieNameArray = ['Oppenheimer', 'They Cloned Tyrone', 'Theater Camp','Heart of Stone', 'Extraction 2']
 const movieDesArray = [
     'During World War II, Lt. Gen. Leslie Groves Jr. appoints physicist J. Robert Oppenheimer to work on the top-secret Manhattan Project. Oppenheimer and a team of scientists spend years developing and designing the atomic bomb.',
-    'When four students play with a magical video game, they are drawn to the jungle world of Jumanji, where they are trapped as their avatars. To return to the real world, they must finish the game.',
-    'Showing Up is a 2022 American comedy-drama film co-written and directed by Kelly Reichardt, in her fourth collaboration with actress Michelle Williams. The film follows a sculptor managing the competing attentions of her art, job, family, and friendships.',
-    'A black-market mercenary who has nothing to lose is hired to rescue the kidnapped son of an imprisoned international crime lord. But in the murky underworld of weapons dealers and drug traffickers, an already deadly mission approaches the impossible.'
+    'They Cloned Tyrone is a 2023 American science fiction comedy mystery film directed by Juel Taylor, in his feature film directorial debut, from a screenplay he wrote with Tony Rettenmaier.',
+    'Theater Camp is a 2023 American comedy film directed by Molly Gordon and Nick Lieberman in both of their respective feature directorial debuts, from a screenplay by Gordon, Lieberman, Ben Platt and Noah Galvin.',
+    'Rachel Stone is an intelligence operative, the only woman who stands between her powerful global peacekeeping organization and the loss of its most valuable -- and dangerous -- asset.',
+    'Extraction 2 (stylised on-screen as Extraction II) is a 2023 American action thriller film directed by Sam Hargrave and written by Joe Russo, based on the graphic novel Ciudad by Ande Parks, Joe Russo, Anthony Russo, Fernando León González, and Eric Skillman.'
 ];
 let videoIndex = 0
 let rightMovie = $.querySelector(".right-movie img")
